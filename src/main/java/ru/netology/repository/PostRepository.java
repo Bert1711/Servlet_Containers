dacced868a6cad45550242d27694d14de4fa2bb8
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-03-26 18:50:49 +0300

    "PostRepository class code refactoring"

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,26 +1,28 @@
 package ru.netology.repository;
 
 import ru.netology.model.Post;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-// Stub
+import java.util.concurrent.ConcurrentHashMap;
+
 public class PostRepository {
+  private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+
   public List<Post> all() {
-    return Collections.emptyList();
+    return List.copyOf(posts.values());
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(posts.get(id));
   }
 
   public Post save(Post post) {
+    posts.put(post.getId(), post);
     return post;
   }
 
   public void removeById(long id) {
-
+    posts.remove(id);
   }
 }
